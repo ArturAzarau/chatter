399d4289df14e527a6e0924ec4fb5ffcf354ac34
Artur Azarau <artur.azarau@gmail.com>
2017-09-04 00:41:46 +0300

    Project created

---
+++ Chatter/Chatter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chatter
+//
+//  Created by Artur Azarov on 04.09.17.
+//  Copyright © 2017 Artur Azarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
